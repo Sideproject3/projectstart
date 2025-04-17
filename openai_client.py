0aa5770c48514fa95510a3ca9372be03cc12e6b6
Atheer <atheers@kth.se>
2025-04-17 13:29:34 +0200

    feat: initial open api call

---
+++ openai_client.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# loading .env file 
+load_dotenv()
+
+client = OpenAI(
+ 	api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+response = client.responses.create(
+	model="gpt-4.1",
+	input="Write a one-sentence bedtime story about a unicorn."
+)
+
+print(response.output_text)
