241a189f7fc31aa8b22bb4aeb75cfe1b1c5f773b
Atheer <atheers@kth.se>
2025-04-22 21:14:52 +0200

    feat: inital testing on google LLMs

---
+++ google_client.py
@@ -0,0 +1,48 @@
+from google import genai
+from google.genai import types
+import pathlib
+import httpx
+
+from dotenv import load_dotenv
+import os
+import argparse
+
+from prompts import SYSTEM_MESSAGE, USER_PROMPT
+from utils.base64_encodings import encode_image, encode_file
+
+parser = argparse.ArgumentParser(prog="Open api image client")
+
+parser.add_argument("-i", "--image", required=True, help="path to input image file")
+parser.add_argument("-g", "--guidelines", required=False, help="path to input guidelines pdf file file")
+# parser.add_argument("-m", "--model", required=True, type=str, choices=["gpt-4.1", "gpt-4.1-mini", "gpt-4.1-nano", "o4-mini", "gpt-4o", "gpt-4o-mini"], help="path to input guidelines pdf file file")
+args = vars(parser.parse_args())
+
+# loading .env file 
+load_dotenv()
+
+with open(args["image"], 'rb') as f:
+	img_bytes = f.read()
+
+pdf_filepath = pathlib.Path(args["guidelines"])
+
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+	model="gemini-2.0-flash",
+	config=types.GenerateContentConfig(
+		system_instruction=SYSTEM_MESSAGE
+	),
+	contents=[
+		types.Part.from_bytes(
+			data=pdf_filepath.read_bytes(),
+			mime_type='application/pdf',
+		),
+		types.Part.from_bytes(
+			data=img_bytes,
+			mime_type='image/jpeg',
+		),
+	USER_PROMPT]
+)
+
+
+print(response.text)
